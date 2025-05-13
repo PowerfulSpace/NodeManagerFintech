1e3bf4d9c0411e94f56bca685489c20e6874a914
Alexander <sany199574@gmail.com>
2025-05-13 21:23:43 +0300

    feat: Добавлены миграции и обновлен appsettings.json

---
+++ src/PS.NodeManagerFintech/PS.NodeManagerFintech.Infrastructure/Persistence/Migrations/20250513174208_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PS.NodeManagerFintech.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExceptionLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp(3) with time zone", precision: 3, nullable: false),
+                    ExceptionType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    RequestPath = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    QueryString = table.Column<string>(type: "text", nullable: true),
+                    Body = table.Column<string>(type: "text", nullable: true),
+                    StackTrace = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExceptionLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TreeNodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    TreeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParentId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreeNodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TreeNodes_TreeNodes_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "TreeNodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TreeNodes_Trees_TreeId",
+                        column: x => x.TreeId,
+                        principalTable: "Trees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExceptionLogs_ExceptionType",
+                table: "ExceptionLogs",
+                column: "ExceptionType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExceptionLogs_Timestamp",
+                table: "ExceptionLogs",
+                column: "Timestamp");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TreeNodes_ParentId",
+                table: "TreeNodes",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TreeNodes_TreeId",
+                table: "TreeNodes",
+                column: "TreeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExceptionLogs");
+
+            migrationBuilder.DropTable(
+                name: "TreeNodes");
+
+            migrationBuilder.DropTable(
+                name: "Trees");
+        }
+    }
+}
